11fad7c0d4aaef4c9b2b6bf7ced6d8b6d33d9baf
kr1ny77 <Kr1ny77@gmail.com>
2025-11-29 18:29:03 +0300

    Implemented a UML diagram without initialization of errors, main and insect classes

---
+++ Assignment4.java
@@ -0,0 +1,133 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Assignment4 {
+    public static void main(String[] args) {
+
+    }
+}
+
+enum Direction {
+    N("North"),
+    E("East"),
+    S("South"),
+    W("West"),
+    NE("North-East"),
+    SE("South-East"),
+    SW("South-West"),
+    NW("North-West");
+
+    private final String textRepresentation;
+    private Direction(String text) {
+        this.textRepresentation = text;
+    }
+
+    public String getTextRepresentation() {
+        return textRepresentation;
+    }
+}
+
+class Board {
+    private final Map<String, BoardEntity> boardData;
+    private final int size;
+
+    public Board(int boardSize) {
+        this.size = boardSize;
+        this.boardData = new HashMap<>();
+    }
+
+    private String toKey(EntityPosition position) {
+        return position.getX() + "," + position.getY();
+    }
+
+    public void addEntity(BoardEntity entity) {
+        boardData.put(toKey(entity.getEntityPosition()), entity);
+    }
+
+    public BoardEntity getEntity(EntityPosition position) {
+        return boardData.get(toKey(position));
+    }
+
+    public Direction getDirection(Insect insect) {
+        return insect.getBestDirection(boardData, size);
+    }
+
+    public int getDirectionSum(Insect insect) {
+        Direction dir = getDirection(insect);
+        return insect.travelDirection(dir, boardData, size);
+    }
+
+}
+
+class EntityPosition {
+    private int x;
+    private int y;
+
+    public EntityPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
+enum InsectColor {
+    RED,
+    GREEN,
+    BLUE,
+    YELLOW;
+
+    public static InsectColor toColor(String s) {
+        switch (s) {
+            case "Red":
+                return RED;
+            case "Green":
+                return GREEN;
+            case "Blue":
+                return BLUE;
+            case "Yellow":
+                return YELLOW;
+            default:
+                throw new InvalidInsectColorException();
+        }
+    }
+}
+
+abstract class BoardEntity {
+    protected EntityPosition entityPosition;
+
+    public BoardEntity(EntityPosition entityPosition) {
+        this.entityPosition = entityPosition;
+    }
+
+    public EntityPosition getEntityPosition() {
+        return entityPosition;
+    }
+}
+
+class FoodPoint extends BoardEntity {
+    protected int value;
+    public FoodPoint(EntityPosition position, int value) {
+        super(position);
+        this.value = value;
+    }
+}
+
+abstract class Insect extends BoardEntity {
+    protected InsectColor color;
+
+    public Insect(EntityPosition entityPosition, InsectColor color) {
+        super(entityPosition);
+        this.color = color;
+    }
+
+    public abstract Direction getBestDirection(Map<String, BoardEntity> boardData, int boardSize);
+
+    public abstract int travelDirection(Direction dir, Map<String, BoardEntity> boardData, int boardSize);
+}
